21d838e22895696b4fa9f8512124ed13014a7bc3
rcamiloo <rodrigo.camilo@gmail.com>
2020-05-15 02:48:58 -0300

    norm on splited read_flags

--- ft_printf/sources/ft_printf.c
+++ ft_printf/sources/ft_printf.c
@@ -6,20 +6,20 @@
 /*   By: rcamilo- <rcamilo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 23:31:23 by rcamilo-          #+#    #+#             */
-/*   Updated: 2020/05/15 02:31:15 by rcamilo-         ###   ########.fr       */
+/*   Updated: 2020/05/15 02:41:05 by camilo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/ft_printf.h"
 
-int	ft_print_flag(t_flags data, va_list args)
+int		ft_print_flag(t_flags data, va_list args)
 {
 	if (data.type == 'd' || data.type == 'i')
 		return (ft_print_d(data, args));
 	return (0);
 }
 
-int	ft_read_flag(t_flags *data, const char *str)
+int		ft_read_flag(t_flags *data, const char *str)
 {
 	int i;
 
@@ -27,7 +27,7 @@ int	ft_read_flag(t_flags *data, const char *str)
 	while ((str[i] == '-' || str[i] == '*' || ft_isnum(str[i])
 				|| str[i] == '.'))
 	{
-		ft_check_flags (str[i], data);
+		ft_check_flags(str[i], data);
 		while (ft_isnum(str[i]))
 		{
 			if (data->point == 0)
@@ -39,33 +39,33 @@ int	ft_read_flag(t_flags *data, const char *str)
 		i++;
 	}
 	if (!(ft_isconversion(str[i])))
-			return (-1);
+		return (-1);
 	data->type = str[i];
 	data->len = i;
 	return (i);
 }
 
-void	ft_check_flags (const char f, t_flags *data)
+void	ft_check_flags(const char f, t_flags *data)
 {
-if (f == '0' && data->minus == 0 && data->width == 0)
-			data->zero = 1;
-		if (f == '-')
-		{
-			data->minus = 1;
-			data->zero = 0;
-		}
-		if (f == '*')
-		{
-			if (data->point == 0)
-				data->star = 1;
-			else
-				data->star = (data->star > 0) ? 3 : 2;
-		}
-		if (f == '.')
-			data->point = 0;
+	if (f == '0' && data->minus == 0 && data->width == 0)
+		data->zero = 1;
+	if (f == '-')
+	{
+		data->minus = 1;
+		data->zero = 0;
+	}
+	if (f == '*')
+	{
+		if (data->point == 0)
+			data->star = 1;
+		else
+			data->star = (data->star > 0) ? 3 : 2;
+	}
+	if (f == '.')
+		data->point = 0;
 }
 
-int	ft_printf(const char *str, ...)
+int		ft_printf(const char *str, ...)
 {
 	va_list	args;
 	t_flags	data;
